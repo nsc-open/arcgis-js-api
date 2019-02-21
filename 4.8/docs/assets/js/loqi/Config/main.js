7b354bbb6b2dcc0d46536dcc6563cb981f9b82a4
ygjack <ygjack414@hotmail.com>
2019-02-21 11:35:04 +0800

    init

---
+++ 4.8/docs/assets/js/loqi/Config/main.js
@@ -0,0 +1,2 @@
+/*! ArcGIS API for JavaScript 4.8 | Copyright (c) 2018 Esri. All rights reserved. | http://www.esri.com/legal/privacy | https://developers.arcgis.com/terms/faq */
+define([],function(){return SiteConfig});
